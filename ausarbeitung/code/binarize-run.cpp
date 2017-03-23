fc3f58f5623258695effd1526042056862871ba1
Armin Wolf <a_wolf28@uni-muenster.de>
2017-03-23 17:02:57 +0100

    zwischenstand

--- ausarbeitung/code/binarize-run.cpp
+++ ausarbeitung/code/binarize-run.cpp
@@ -1,20 +1,12 @@
 Mat ImageBinarization::run(Mat image, int thresholdMethod) {
-
-	this->image = image;
 	computeSmoothing();
-	createHistogram();
-
-	printThresholdMethod(thresholdMethod);
 	switch (thresholdMethod) {
 	case Global:
-		setThresholdValue();
 		computeGlobalThreshold();
 		break;
-
 	case LocalMean:
 		computeLocalThreshold(ADAPTIVE_THRESH_MEAN_C, 11, 0);
 		break;
-
 	case LocalGaussian:
 		computeLocalThreshold(ADAPTIVE_THRESH_GAUSSIAN_C, 11, 0);
 		break;
